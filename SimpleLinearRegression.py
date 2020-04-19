80b8b80f40adf284d0690c16b3f866c954e8fa22
lucamondini <lucamondini78@gmail.com>
2020-04-19 18:56:12 +0200

    Update optimal values

--- SimpleLinearRegression.py
+++ SimpleLinearRegression.py
@@ -93,8 +93,8 @@ print("Defined the plot_the_model and plot_the_loss_curve functions.")
 my_feature = ([1.0, 2.0,  3.0,  4.0,  5.0,  6.0,  7.0,  8.0,  9.0, 10.0, 11.0, 12.0])
 my_label   = ([5.0, 8.8,  9.6, 14.2, 18.8, 19.5, 21.4, 26.8, 28.9, 32.0, 33.8, 38.2])
 
-learning_rate=0.01
-epochs=10
+learning_rate=0.14
+epochs=70
 my_batch_size=12
 
 my_model = build_model(learning_rate)
